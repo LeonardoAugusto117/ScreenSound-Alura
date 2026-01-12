04e39bb19c8ef34029a746f6d44983f4d253629a
leonardo CASA <leonardodevaugusto2d@gmail.com>
2026-01-12 15:56:44 -0300

    Subindo desafio

---
+++ ScreenSounf/ScreenSounf/Convidados.cs
@@ -0,0 +1,12 @@
+﻿class Convidados
+{
+    public string Nome { get; set; }
+    public string Profissao { get; set; }
+    public int Idade { get; set; }
+    public Convidados(string nome, string profissao, int idade)
+    {
+        Nome = nome;
+        Profissao = profissao;
+        Idade = idade;
+    }
+}
--- ScreenSounf/ScreenSounf/Program.cs
+++ ScreenSounf/ScreenSounf/Program.cs
@@ -11,7 +11,7 @@ Musica musica1 = new Musica(queen, "Bohemian Rhapsody")
 Musica musica2 = new Musica(queen, "Don't Stop Me Now")
 {
     Duracao = 210,
-    Disponivel = true  
+    Disponivel = true
 
 };
 
@@ -24,3 +24,21 @@ musica2.ExibirFichaTecnica();
 
 queen.AdicionarAlbum(albumQuenn);
 queen.ExibirAlbuns();
+
+// Podecast Desafio
+
+Console.WriteLine("\n--- Podcast Desafio ---\n");
+
+Episodio ep1 = new(2, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidado("Maria");
+ep1.AdicionarConvidado("Marcelo");
+
+Episodio ep2 = new(1, "Tecnicas de aprendizado", 67);
+ep2.AdicionarConvidado("Fernando");
+ep2.AdicionarConvidado("Marcos");
+ep2.AdicionarConvidado("Flavia");
+
+Podcast podcast = new("Podcast especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
